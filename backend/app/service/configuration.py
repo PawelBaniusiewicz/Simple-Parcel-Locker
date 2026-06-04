80e4101f0f92336b2e3f10b7848b94f7abb07938
PawelBaniusiewicz <pawel.baniusiewicz0@gmail.com>
2026-06-04 09:13:47 +0200

    feat(backend): fixed broken import

--- backend/app/service/configuration.py
+++ backend/app/service/configuration.py
@@ -1,5 +1,5 @@
 from .user_service import UserService
 from ..db.repository import user_repository
-from app.db.repository import activation_toke_repository
+from app.db.repository import activation_token_repository
 
-user_service = UserService(user_repository, activation_toke_repository)
+user_service = UserService(user_repository, activation_token_repository)
