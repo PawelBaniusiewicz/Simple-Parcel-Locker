04edf350bc31385dd5004f64e55bebe14aeb6729
PawelBaniusiewicz <pawel.baniusiewicz0@gmail.com>
2026-07-01 16:57:20 +0200

    deleted useless imports

--- backend/main.py
+++ backend/main.py
@@ -3,8 +3,6 @@ from flask_restful import Api
 from flask_cors import CORS
 from flask_migrate import Migrate
 
-from dotenv import load_dotenv
-from pathlib import Path
 from os import getenv
 import logging
 
