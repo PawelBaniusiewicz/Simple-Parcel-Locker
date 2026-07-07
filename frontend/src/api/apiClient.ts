cc3673cdb4e67a50061575269edb31517aa39849
PawelBaniusiewicz <pawel.baniusiewicz0@gmail.com>
2026-07-07 16:14:47 +0200

    Added new flag to apiClient (withCredentials: true)

--- frontend/src/api/apiClient.ts
+++ frontend/src/api/apiClient.ts
@@ -4,7 +4,8 @@ const apiClient = axios.create({
     baseURL: import.meta.env.REACT_APP_BASE_API_URL,
     headers: {
         'Content-Type': 'application/json'
-    }
+    },
+    withCredentials: true
 });
 
 export default apiClient;
