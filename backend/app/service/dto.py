7da4ad863db0a8f8398f5741f59b6e2f40245ad0
PawelBaniusiewicz <pawel.baniusiewicz0@gmail.com>
2026-08-05 15:30:47 +0200

    Feat(backend): Added ParcelLockerDto

--- backend/app/service/dto.py
+++ backend/app/service/dto.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 from typing import Self, Any
 
-from app.db.entity import UserEntity, ParcelEntity
+from app.db.entity import UserEntity, ParcelEntity, ParcelLockerEntity
 from ..models.enums import Size, Status
 
 import datetime
@@ -113,4 +113,31 @@ class ParcelDto:
             parcel_entity.locker_id,
             parcel_entity.receiver_id,
             parcel_entity.sender_id
+        )
+
+@dataclass
+class ParcelLockerDto:
+    id: int
+    name: str
+    address: str
+    latitude: float
+    longitude: float
+
+    def to_dict(self) -> dict[str, Any]:
+        return {
+            'id': self.id,
+            'name': self.name,
+            'address': self.address,
+            'latitude': self.latitude,
+            'longitude': self.longitude
+        }
+
+    @classmethod
+    def from_parcel_locker_entity(cls, parcel_locker_entity: ParcelLockerEntity) -> Self:
+        return cls(
+            parcel_locker_entity.id,
+            parcel_locker_entity.name,
+            parcel_locker_entity.address,
+            parcel_locker_entity.latitude,
+            parcel_locker_entity.longitude
         )
