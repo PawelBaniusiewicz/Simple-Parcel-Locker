a6ed4a9563cc46e2b03e2ba2e89052610f2222be
PawelBaniusiewicz <pawel.baniusiewicz0@gmail.com>
2026-08-05 16:55:46 +0200

    Feat(backend): Added Service, Repository, Resource for ParcelLockers

--- backend/app/service/configuration.py
+++ backend/app/service/configuration.py
@@ -1,6 +1,8 @@
-from app.db.repository import activation_token_repository, user_repository, parcel_repository, locker_repository
+from app.db.repository import activation_token_repository, user_repository, parcel_repository, parcel_locker_repository
+from .parcel_locker_service import ParcelLockerService
 from .parcel_service import ParcelService
 from .user_service import UserService
 
 user_service = UserService(user_repository, activation_token_repository)
 parcel_service = ParcelService(parcel_repository)
+parcel_locker_service = ParcelLockerService(parcel_locker_repository)
---
+++ backend/app/service/parcel_locker_service.py
@@ -0,0 +1,38 @@
+from dataclasses import dataclass
+
+from app.db.entity import ParcelLockerEntity
+from app.db.repository import ParcelLockerRepository
+from app.service.dto import ParcelLockerDto
+
+
+@dataclass
+class ParcelLockerService:
+    parcel_locker_repo: ParcelLockerRepository
+
+    def get_parcel_locker_by_id(self, parcel_locker_id: int) -> ParcelLockerDto | None:
+        parcel_locker = self.parcel_locker_repo.find_by_id(parcel_locker_id)
+
+        if not parcel_locker:
+            raise ValueError(f'Cannot find parcel locker with ID {parcel_locker_id}')
+
+        return ParcelLockerDto.from_parcel_locker_entity(parcel_locker)
+
+    def get_all_parcel_lockers(self) -> list['ParcelLockerDto']:
+        parcel_lockers = self.parcel_locker_repo.find_all()
+
+        if not parcel_lockers:
+            return []
+
+        return [ParcelLockerDto.from_parcel_locker_entity(pl) for pl in parcel_lockers]
+
+    def add_parcel_locker(self, name: str, address: str, latitude: float, longitude: float) -> 'ParcelLockerDto':
+        parcel_locker = ParcelLockerEntity(
+            name=name,
+            address=address,
+            latitude=latitude,
+            longitude=longitude
+        )
+
+        self.parcel_locker_repo.save_or_update(parcel_locker)
+
+        return ParcelLockerDto.from_parcel_locker_entity(parcel_locker)
